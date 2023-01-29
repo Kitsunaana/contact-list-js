73a99c0efc29e99bb3d94aadd6bc42507538ca54
Selimkhanov Deni <selimkhanov_deni@list.ru>
2023-01-29 12:12:44 +0700

    #1 Настройка client/server

---
+++ client/js/createHeader.js
@@ -0,0 +1,28 @@
+export const createClientsHeader = () => {
+    const header = document.createElement('header')
+    const logo = document.createElement('a')
+    const logoImg = document.createElement('img')
+    const form = document.createElement('form')
+    const input = document.createElement('input')
+    const container = document.createElement('div')
+    const wrapper = document.createElement('div')
+    const inner = document.createElement('div')
+
+    header.classList.add('header')
+    logo.classList.add('logo', 'header__logo')
+    logoImg.classList.add('logo__img')
+    logoImg.src = './img/logo.png'
+    form.classList.add('header__form')
+    input.classList.add('header__input')
+    input.placeholder = 'Введите запрос'
+    wrapper.classList.add('header__wrapper')
+    inner.classList.add('header__inner')
+    container.classList.add('container', 'header__container')
+
+    header.append(container)
+    logo.append(logoImg)
+    form.append(input)
+    container.append(logo, form)
+
+    return header
+}
